0fe65cb9026b563fa490b41253c88cceea8a612b
Tomislav Nikic <tomislav@nikic.org>
2026-03-23 17:27:04 +0100

    2026

--- server/files/edit.js
+++ server/files/edit.js
@@ -1,9 +1,8 @@
 function setMovie(movie) {
-
   for (const element of document.forms[0].elements) {
     const name = element.id;
     const value = movie[name];
-    
+
     if (name === "Genres") {
       const options = element.options;
       for (let index = 0; index < options.length; index++) {
@@ -19,7 +18,9 @@ function setMovie(movie) {
 function getMovie() {
   const movie = {};
 
-  const elements = Array.from(document.forms[0].elements).filter(element => element.id)
+  const elements = Array.from(document.forms[0].elements).filter(
+    (element) => element.id,
+  );
 
   for (const element of elements) {
     const name = element.id;
@@ -35,9 +36,17 @@ function getMovie() {
           value.push(option.value);
         }
       }
-    } else if (name === "Metascore" || name === "Runtime" || name === "imdbRating") {
-        value = Number(element.value);
-    } else if (name === "Actors" || name === "Directors" || name === "Writers") {
+    } else if (
+      name === "Metascore" ||
+      name === "Runtime" ||
+      name === "imdbRating"
+    ) {
+      value = Number(element.value);
+    } else if (
+      name === "Actors" ||
+      name === "Directors" ||
+      name === "Writers"
+    ) {
       value = element.value.split(",").map((item) => item.trim());
     } else {
       value = element.value;
@@ -58,16 +67,14 @@ function putMovie() {
     - Send the movie data as JSON
   */
 
-  const xhr = new XMLHttpRequest()
-  xhr.onload = function() {
+  const xhr = new XMLHttpRequest();
+  xhr.onload = function () {
     if (xhr.status == 200 || xhr.status === 204) {
-      location.href = 'index.html'
+      location.href = "index.html";
     } else {
-      alert("Saving of movie data failed. Status code was " + response.status)
+      alert("Saving of movie data failed. Status code was " + xhr.status);
     }
-  }
-  
-
+  };
 }
 
 /** Loading and setting the movie data for the movie with the passed imdbID */
@@ -75,12 +82,18 @@ const imdbID = new URLSearchParams(window.location.search).get("imdbID");
 
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "/movies/" + imdbID);
-xhr.onload = function() {
+xhr.onload = function () {
   if (xhr.status === 200) {
     setMovie(JSON.parse(xhr.responseText));
   } else {
-    alert("Loading of movie data failed. Status was " + xhr.status + " - " + xhr.statusText);
-  } 
-}
+    alert(
+      "Loading of movie data failed. Status was " +
+        xhr.status +
+        " - " +
+        xhr.statusText,
+    );
+  }
+};
 
-xhr.send()
+xhr.send();
+
